608765c5551b987540c0882c18cf7f7e4b5c423b
Suman Dey <sumandey7684@gmail.com>
2025-09-07 23:34:06 +0530

    Update personalInfo.ts

--- src/Data/personalInfo.ts
+++ src/Data/personalInfo.ts
@@ -1,6 +1,6 @@
 export const personalData = {
     Name: "Suman Dey",
-    Alias: "sumxnnn_911",
+    Alias: "sumxn.911",
     ProfilePicture: "https://avatars.githubusercontent.com/sumandey7684",
   aboutMe:
     "I'm a full stack developer who enjoys solving real problems with code. I mostly work on web apps, but I'm also interested in other areas like real-time systems, command-line tools, and AI projects. I like learning new things and building simple, useful tools that actually help people.",
